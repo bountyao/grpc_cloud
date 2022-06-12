f9a51d0d349ee4a1e18751595c2400c30550e955
CzarCerro <abramobligacion@gmail.com>
2022-06-12 10:48:38 +0800

    feat: add more services
    Added more services in the proto file

--- grpc-services/client.py
+++ grpc-services/client.py
@@ -6,20 +6,29 @@ import tracetogether_pb2
 import tracetogether_pb2_grpc
 import datetime
 
-def run():
-    with grpc.insecure_channel('localhost:50051') as channel:
-        stub = tracetogether_pb2_grpc.TraceTogetherStub(channel)
+class Client:
+    stub = None
 
-        response = stub.CheckIn(tracetogether_pb2.Request(name='Bob', nric='S1234567A', location='NYP', time=str(datetime.datetime.now())))
-        print("Client received: " + response.message)
+    def __init__(self):
+        print('1. Login as user\n'
+              '2. Login as officer\n'
+              '3. Quit')
+        userInput = input()
 
-        responses = stub.Test(tracetogether_pb2.Request())
-        for response in responses:
-            print(response.message)
+        if userInput == '3':
+            exit(1)
 
+    def run(self):
+        with grpc.insecure_channel('localhost:50051') as channel:
+            stub = tracetogether_pb2_grpc.TraceTogetherStub(channel)
+            response = stub.CheckIn(tracetogether_pb2.Request(name='Bob', nric='S1234567A', location='NYP', time=str(datetime.datetime.now())))
+            print("Client received: " + response.message)
 
+            responses = stub.Test(tracetogether_pb2.Request())
+            for response in responses:
+                print(response.message)
 
 
 if __name__ == '__main__':
     logging.basicConfig()
-    run()
+    Client().run()
